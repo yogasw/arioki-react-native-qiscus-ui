a050fbf7596365e9d275e724986ce15c3f8dd27a
yoga setiawan <yogainformatika@gmail.com>
2019-10-26 10:21:13 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {ListChatRoom} from 'react-native-qiscus-ui';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <ListChatRoom/>
+    </>
+  );
+};
+
+
+export default App;
