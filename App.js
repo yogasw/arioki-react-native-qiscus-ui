7aba18b101db2f17367b6cdb89577ab8d7c32825
yoga setiawan <yogainformatika@gmail.com>
2019-10-26 16:03:58 +0700

    add function get room list

--- App.js
+++ App.js
@@ -7,15 +7,71 @@
  */
 
 import React from 'react';
-import {ListChatRoom} from 'react-native-qiscus-ui';
+import {Text, View, Button} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {ListChatRoom, QiscusInit, QiscusLoginRegister} from 'react-native-qiscus-ui';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <ListChatRoom/>
-    </>
-  );
-};
+class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+    };
+  }
+
+  register = () => QiscusLoginRegister('testRn', 'testRn', 'testRn');
+
+  render() {
+    return (
+      <>
+        <Button title={'Login'} onPress={() => this.register()}/>
+
+        <Button title={'Home'} onPress={() => {
+          this.props.navigation.navigate('ChatScreen');
+        }} style={{marginTop: 20}}/>
+      </>
+    );
+
+  }
+}
+
+class ChatScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <ListChatRoom/>
+      </>
+    );
+
+  }
+}
+
+const AppNavigator = createAppContainer(createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+  ChatScreen: {
+    screen: ChatScreen,
+  },
+}));
+
+class App extends React.Component {
+  componentDidMount() {
+    QiscusInit();
+  }
+
+  render() {
+    return <AppNavigator/>;
+  }
+}
 
 
 export default App;
